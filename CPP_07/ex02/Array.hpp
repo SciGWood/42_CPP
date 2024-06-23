df55fdca718653fa9f6f253077f00db0df22a2f8
PC3_Gwen <gdetourn@student.42perpignan.fr>
2024-06-23 16:41:45 +0200

    CPP_07_ex02_OK

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -3,6 +3,8 @@
 
 # include <iostream>
 # include <cstdlib>
+# include <stdint.h>
+# include <exception>
 
 # define RESET "\033[0m"
 # define BLUE "\033[94m"
@@ -12,22 +14,90 @@
 # define MAGENTA "\033[35m"
 # define CYAN "\033[36m"
 
+template <typename T>
 class Array
 {
-private:
-	/* data */
-public:
-	Array(/* args */);
-	~Array();
+	private:
+		T				*_array;
+		unsigned int	_size;
+	public:
+		Array();//										Default Constructor
+		Array(unsigned int size);
+		Array(const Array &other);//				    Copy constructor
+		Array&	operator=(const Array &other);//	    Copy assignement operator
+		~Array();//										Destructor
+
+		T				&operator[](unsigned int i) const;
+		unsigned int	size() const;
+
+		class OutOfBoundsException : public std::exception
+		{
+			public:
+				virtual const char	*what(void) const throw();
+		};
 };
 
-Array::Array(/* args */)
+template <typename T>
+Array<T>::Array() : _array(new T[0]()), _size(0)
 {
+
 }
 
-Array::~Array()
+template <typename T>
+Array<T>::Array(unsigned int size) : _array(new T[size]()), _size(size)
 {
+
 }
 
+template <typename T>
+Array<T>::Array(const Array &other)
+{
+	if (this != &other)
+	{
+		this->_size = other._size;
+		this->_array = new T[other._size];
+		for (unsigned int i = 0; i < this->_size; i++)
+			this->_array[i] = other._array[i];
+	}
+}
+template <typename T>
+Array<T>&	Array<T>::operator=(const Array &other)
+{
+	if (this != &other)
+	{
+		this->_size = other._size;
+		delete [] this->_array;
+		this->_array = new T[other._size];
+		for (unsigned int i = 0; i < this->_size; i++)
+			this->_array[i] = other._array[i];
+	}
+	return (*this);
+}
+
+template <typename T>
+T	&Array<T>::operator[](unsigned int i) const
+{
+	if (i >= this->_size)
+		throw (Array<T>::OutOfBoundsException());
+	return (this->_array[1]);
+}
+
+template <typename T>
+unsigned int	Array<T>::size() const
+{
+	return (this->_size);
+}
+
+template <typename T>
+const char	*Array<T>::OutOfBoundsException::what() const throw()
+{
+	return ("\nOut of Bounds Exception.");
+}
+
+template <typename T>
+Array<T>::~Array()
+{
+	delete [] this->_array;
+}
 
 #endif
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gwen <gwen@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 10:32:33 by gwen              #+#    #+#             */
-/*   Updated: 2024/06/23 10:34:43 by gwen             ###   ########.fr       */
+/*   Updated: 2024/06/23 16:36:06 by gwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,45 @@
 
 int	main()
 {
+	{
+		Array<int>	arr(6);
+		for (unsigned int i = 0; i < arr.size(); i++)
+			std::cout << "Array[" << i << "] = " << arr[i] << std::endl;
+		arr[0] = 5;
+		arr[1] = 10;
+		arr[2] = 25;
+		arr[3] = 18;
+		arr[4] = 32;
+		arr[5] = 28;
+		for (unsigned int i = 0; i < arr.size(); i++)
+			std::cout << "Array[" << i << "] = " << arr[i] << std::endl;
+	}
+
+	{
+		Array<int>	arr(3);
+		try
+		{
+			arr[3] = 6;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n' << std::endl;
+		}
+	}
+
+	{
+		Array<std::string>	arr(6);
+		for (unsigned int i = 0; i < arr.size(); i++)
+			std::cout << "Array[" << i << "] = " << arr[i] << std::endl;
+		arr[0] = "Hello";
+		arr[1] = "World";
+		arr[2] = "Dans";
+		arr[3] = "le";
+		arr[4] = "Train";
+		arr[5] = "toujours en RETARD !!";
+		for (unsigned int i = 0; i < arr.size(); i++)
+			std::cout << "Array[" << i << "] = " << arr[i] << std::endl;
+	}
+
 	return (0);
 }
