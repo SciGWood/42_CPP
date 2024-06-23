aa869d9a587714714548bba2be5f75d2e247e9db
PC3_Gwen <gdetourn@student.42perpignan.fr>
2024-06-23 11:16:44 +0200

    CPP_07_ex01

---
+++ CPP_07/ex01/iter.hpp
@@ -0,0 +1,37 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+# include <cstdlib>
+
+# define RESET "\033[0m"
+# define BLUE "\033[94m"
+# define RED "\033[91m"
+# define GREEN "\033[92m"
+# define YELLOW "\033[33m"
+# define MAGENTA "\033[35m"
+# define CYAN "\033[36m"
+
+// template <typename T>
+// void	crypto(T &toHide)
+// {
+// 	int	code;
+
+// 	code = toHide + 5;
+// 	print(code);
+// }
+
+template <typename T>
+void	print(T const &toPrint)
+{
+	std::cout << toPrint << std::endl;
+}
+
+template <typename T>
+void	iter(T *ArrayPtr, size_t len, void(*f)(const T &))
+{
+	for (size_t i = 0; i < len; i++)
+		f(ArrayPtr[i]);
+}
+
+#endif
---
+++ CPP_07/ex02/Array.tpp
@@ -0,0 +1,5 @@
+#ifndef ARRAY_TPP
+# define ARRAY_TPP
+
+
+#endif
