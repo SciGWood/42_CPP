447ddb015d52f1fb91a99bb6976681e5d56afecc
Gwenaelle Detourne <gdetourn@c3r2p9.42perpignan.fr>
2024-06-26 17:10:16 +0200

    CPP_09_start

---
+++ CPP_09/ex00/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdetourn <gdetourn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:07:34 by gdetourn          #+#    #+#             */
+/*   Updated: 2024/06/26 16:27:36 by gdetourn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3 || argv[1] diff de .txt)
+    {
+        std::cout << "This program needs a file.csv and a file.txt as argument\n" << std::endl;
+    }
+    return (0);
+}
